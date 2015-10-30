b2c30ed2a18faf1ae6b2294883783d9ec71126b1
Marcelo <Marcelo@MarceloHP>
2015-10-30 00:56:48 -0500

    creee los DTO o beans

---
+++ src/main/java/pucp/sw2/proyecto/etesis0891/DTO/PersonaDTO.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pucp.sw2.proyecto.etesis0891.DTO;
+
+/**
+ *
+ * @author Marcelo
+ */
+public class PersonaDTO {
+    private int idpersona;
+        private String nombre;
+    private String apelidos;
+    private String codigo;
+    private String password;
+    private String DNI;
+    private String correo;
+    private String Telefono;
+    private String estado;
+    private String phone;
+
+    private int idrol;
+
+    public int getIdpersona() {
+        return idpersona;
+    }
+
+    public void setIdpersona(int idpersona) {
+        this.idpersona = idpersona;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApelidos() {
+        return apelidos;
+    }
+
+    public void setApelidos(String apelidos) {
+        this.apelidos = apelidos;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getIdrol() {
+        return idrol;
+    }
+
+    public void setIdrol(int idrol) {
+        this.idrol = idrol;
+    }
+    
+    
+    
+}
