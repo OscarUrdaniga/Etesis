f6e2376d62539dd2c6fe74a43046094897831d2a
gtrpucp <gtrpucp@localhost>
2015-11-03 10:39:01 -0500

    creando Daos

---
+++ src/main/java/pucp/sw2/proyecto/etesis0891/dao/personaDao.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pucp.sw2.proyecto.etesis0891.dao;
+
+import java.util.List;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import pucp.sw2.proyecto.etesis0891.DTO.Persona;
+
+/**
+ *
+ * @author gtrpucp
+ */
+public class personaDao {
+    @Autowired
+    private DataSource dataSource;
+    
+    public List<Persona> listaPersonas(){
+    
+        
+        
+        
+        
+        return null;
+    
+    
+    
+    } 
+}
